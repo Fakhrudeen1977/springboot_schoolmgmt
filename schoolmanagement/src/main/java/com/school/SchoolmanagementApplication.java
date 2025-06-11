5600ca12150d016d257d2a5f9f5ca56c221ea141
Fakhrudeen1977 <fakhrudeen77@gmail.com>
2025-06-11 19:51:56 +0530

    Logs Added in Main Application

--- schoolmanagement/src/main/java/com/school/SchoolmanagementApplication.java
+++ schoolmanagement/src/main/java/com/school/SchoolmanagementApplication.java
@@ -1,19 +1,23 @@
 package com.school;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
+import com.school.controller.MasterController;
 @SpringBootApplication
 
 public class SchoolmanagementApplication extends SpringBootServletInitializer {
+	
+	private static final Logger logger = LoggerFactory.getLogger(SchoolmanagementApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("Spring Boot Application Loading");
 		
-		SpringApplication.run(SchoolmanagementApplication.class, args);
-		
+		SpringApplication.run(SchoolmanagementApplication.class, args);	
 		
 		
 				
